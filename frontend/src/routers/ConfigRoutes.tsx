7c135198b30f8662a27c0c43228bb93bdadfc4b0
Sipprawit Promsa <sippravit5003@gmail.com>
2025-09-10 14:10:18 +0700

    แก้ config

--- frontend/src/routers/ConfigRoutes.tsx
+++ frontend/src/routers/ConfigRoutes.tsx
@@ -11,8 +11,8 @@ import SignUpPage from "../pages/auth/Register";
 import Dashboard from "../pages/dashboard";
 import Booking from "../pages/classbooking/classHome";
 import TrainerBooking from "../pages/trainer/trainer/trainerbooking"
-import Health from "../pages/health/Health/HealthHome";
-import Activity from "../pages/health/Activity/ActivityHome";
+import HealthHome from "../pages/health/Health/healthHome";
+import Nutrition from "../pages/health/nutrition/nutritionHome";
 import Group from "../pages/group/groupHome";
 import Package from "../pages/package/packageHome";
 import Customer from "../pages/customer";
@@ -28,6 +28,10 @@ import TrainerProfile from "../pages/trainer/Actor/trainerProfile";
 import ManageSchedule from "../pages/admin/class-activity/class-activityHome";
 import ManageEquipment from "../pages/admin/equipment/equipmentHome";
 import UserList from "../pages/admin/List/userlist";
+import { HealthActivityProvider } from "../contexts/HealthContext";
+import { NutritionProvider } from "../contexts/NutritionContext";
+
+
 
 
 const ConfigRoutes: React.FC = () => {
@@ -45,9 +49,18 @@ const ConfigRoutes: React.FC = () => {
         <Route path="/" element={<Dashboard />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/trainerbooking" element={<TrainerBooking />} />
-        <Route path="/health" element={<Health />} />
-        <Route path="/health/Health" element={<Health />} />
-        <Route path="/health/Activity" element={<Activity />} />
+        <Route path="/health/Health" element={
+          <HealthActivityProvider>
+            <HealthHome />
+          </HealthActivityProvider>
+        } />
+        <Route path="/health/nutrition" element={
+          <HealthActivityProvider>
+            <NutritionProvider>
+              <Nutrition />
+            </NutritionProvider>
+          </HealthActivityProvider>
+        } />
         <Route path="/group" element={<Group />} />
         <Route path="/package" element={<Package />} />
         <Route path="/customer" element={<Customer />} />
